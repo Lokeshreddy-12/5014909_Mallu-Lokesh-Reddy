42a581df9bbd15f966f0e8e853b48e5caee46c67
Lokeshreddy-12 <mallulokeshreddy21@gmail.com>
2025-08-16 23:06:35 +0530

    Solved New year chaos problem

---
+++ HackerRank/Week 3/New Year chaos.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+void minimumBribes(vector<int> v) {
+    int swaps=0;
+    int flag=0;
+    for(int i=v.size()-1;i>=0;i--){
+        int key = v[i];
+        int j=i+1;
+        int max_limit=0;
+        while((j<v.size()) && (key>v[j])){
+            if(max_limit<2){
+                v[j-1]= v[j];
+                j++;
+                max_limit++;
+                swaps++;
+            }
+            else{
+                flag++;
+                break;
+            }
+        }
+        if(!flag)
+            v[j-1]= key;
+        else{
+            break;
+        }
+    }
+   if(!flag)  cout << swaps <<"\n";
+   else       cout << "Too chaotic" <<"\n";
+}
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string q_temp_temp;
+        getline(cin, q_temp_temp);
+
+        vector<string> q_temp = split(rtrim(q_temp_temp));
+
+        vector<int> q(n);
+
+        for (int i = 0; i < n; i++) {
+            int q_item = stoi(q_temp[i]);
+
+            q[i] = q_item;
+        }
+
+        minimumBribes(q);
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
