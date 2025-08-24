33bd750958ebd716edc5fd43233ca1feaeb52bfb
Lokeshreddy-12 <mallulokeshreddy21@gmail.com>
2025-08-24 20:55:27 +0530

    Solved Equal Stacks, The Maximum Subarray problems

---
+++ HackerRank/Week4/The Maximum Subarray.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+vector<int> maxSubarray(vector<int> arr) {
+    int maxsum=INT_MIN;
+    int currsum=0;
+    int subsum=INT_MIN;
+    for(int i=0;i<arr.size();i++){
+        currsum+=arr[i];
+        maxsum = max(maxsum,currsum);
+        if(currsum<0){
+            currsum=0;
+        }
+        if(arr[i]>0){
+            if(subsum<0) subsum=0;
+            subsum+=arr[i];
+        }else if(subsum<0){
+            if(subsum<arr[i]) subsum=arr[i];
+        }
+    }
+    vector<int>ans;
+    ans.push_back(maxsum);
+    ans.push_back(subsum);
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        vector<int> result = maxSubarray(arr);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
